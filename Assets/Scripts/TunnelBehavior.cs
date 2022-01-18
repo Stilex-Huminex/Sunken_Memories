fa51833e2b29ff64b6cd7522ab09d9de52e59043
1Samper1 <quentin.arzalier@free.fr>
2022-01-18 01:46:37 +0100

    Avancée Test Sub

---
+++ Assets/Scripts/TunnelBehavior.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelBehavior : MonoBehaviour
+{
+    [SerializeField] private Transform other;
+    private float _startZ;
+    private float _diff;
+
+    private void Start()
+    {
+        _startZ = transform.position.z;
+        _diff = other.position.z - _startZ;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.back*0.2f);
+        other.Translate(Vector3.back*0.2f);
+        if (!(other.position.z < _startZ)) return;
+        transform.Translate(Vector3.forward*_diff);
+        other.Translate(Vector3.forward*_diff);
+    }
+}
--- Assets/Scripts/TunnelMovement.cs
+++ Assets/Scripts/TunnelMovement.cs
@@ -6,14 +6,44 @@ public class TunnelMovement : MonoBehaviour
     private const float MoveSpeed = 20f;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Transform helice;
+    [SerializeField] private ParticleSystem smoke;
+    private bool canMove = true;
+    private float stunTimer;
+    private bool hasCrashed;
 
     private void FixedUpdate()
     {
+        var pos = transform.position;
+        if (hasCrashed)
+        {
+            transform.position += UnityEngine.Random.insideUnitSphere * 0.1f * new Vector3(1f, 1f, 0f);
+        }
+        if (!canMove)
+        {
+            stunTimer -= Time.deltaTime;
+            if (!(stunTimer <= 0)) return;
+            rb.drag = 20f;
+            canMove = true;
+            return;
+        }
         var vel = rb.velocity;
         var h = Input.GetAxis("Horizontal") * MoveSpeed;
         var v = Input.GetAxis("Vertical") * MoveSpeed;
         rb.AddForce(new Vector3(h, v, 0), ForceMode.Impulse);
         helice.Rotate(Vector3.left, (float)Math.Sqrt(vel.x*vel.x + vel.y*vel.y)*0.2f + 4f, Space.Self);
         rb.velocity = Vector3.ClampMagnitude(vel, 10);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Tunnel"))
+        {
+            hasCrashed = true;
+            smoke.Play();
+            stunTimer = 1f;
+            canMove = false;
+            rb.drag = 3f;
+            rb.AddForce(transform.InverseTransformPoint(Vector3.zero)*2f, ForceMode.VelocityChange);
+        }
     }
 }
