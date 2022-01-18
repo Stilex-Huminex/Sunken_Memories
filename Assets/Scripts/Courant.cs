5db49e894198f9de2a9cff5ff3694d3bb8c1bc60
LoicGestin <loic.gestin34@gmail.com>
2022-01-18 16:54:06 +0100

    Courant update

--- Assets/Scripts/Courant.cs
+++ Assets/Scripts/Courant.cs
@@ -7,17 +7,26 @@ public class Courant : MonoBehaviour
     GameObject SubMarine;
     public Rigidbody rb;
     public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
+    public float backForce = 0f;
+    public float rightForce = 500f;
+    public float leftForce = 0f;
+    public float upForce = 0f;
+    public float downForce = 0f;
 
 
-    
+
+
     private void OnTriggerStay(Collider collider)
         {
             if (collider.gameObject.name.Equals("Submarine"))
             {
-                rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-                rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-             }
+                rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+                rb.AddForce(0, 0, - backForce * Time.deltaTime, ForceMode.VelocityChange);
+                rb.AddForce(rightForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+                rb.AddForce(-leftForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+                rb.AddForce(0, upForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+                rb.AddForce(0, - downForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
         }
 }
 
