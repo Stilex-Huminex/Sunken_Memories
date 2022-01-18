3d3023827bb099364cb0ce1d62b90b23210c66dd
LoicGestin <loic.gestin34@gmail.com>
2022-01-18 15:26:41 +0100

    Courant

---
+++ Assets/Scripts/Courant.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Courant : MonoBehaviour
+{
+    GameObject SubMarine;
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+
+    
+    private void OnTriggerStay(Collider collider)
+        {
+            if (collider.gameObject.name.Equals("Submarine"))
+            {
+                rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+                rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+                rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+             }
+        }
+}
+
+
